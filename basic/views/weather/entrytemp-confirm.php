641dacea7dc6b413ff2cb431e00f33732a8b2750
Valery Simonov <simonvn@yandex.ru>
2017-06-15 08:43:14 +0300

    Delete entrytemp-confirm.php

--- basic/views/weather/entrytemp-confirm.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<p>Вы ввели следующую информацию:</p>
-
-<ul>
-    <li><label>From</label>: <?= Html::encode($model->from) ?></li>
-    <li><label>To</label>: <?= Html::encode($model->to) ?></li>
-</ul>
